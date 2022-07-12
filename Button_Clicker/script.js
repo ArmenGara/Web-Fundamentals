f1dc9013840580f17034c74d063952f8d490f6d2
ArmenGara <36677601+ArmenGara@users.noreply.github.com>
2022-07-12 12:33:11 -0700

    Add files via upload

---
+++ Button_Clicker/script.js
@@ -0,0 +1,11 @@
+function logOut(element) {
+    element.innerText = "Log Out";
+}
+
+function remove(element) {
+    element.remove();
+}
+
+function hello(element) {
+    alert("Ninja was liked");
+}
